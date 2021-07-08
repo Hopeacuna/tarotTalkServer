1f92140dc0a543f57f0e3bdd08c2ca2ce44579bc
Hopeacuna <esperanzahopeacuna@gmail.com>
2021-07-08 09:08:26 -0400

    heroku

--- db.js
+++ db.js
@@ -1,5 +1,10 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize("postgres://postgres:password@localhost:5432/tarot");
+// const sequelize = new Sequelize("postgres://postgres:password@localhost:5432/tarot");
+
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres',
+    ssl: process.env.ENVIRONMENT === 'production'
+});
 
 module.exports = sequelize;
